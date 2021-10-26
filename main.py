1340317d1b35070c331c82c77c743cf6f7c46d7c
Asaf <asafbm95@gmail.com>
2021-10-26 20:17:19 +0000

    Last V for 26/10

--- main.py
+++ main.py
@@ -4,13 +4,12 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import pandas as pd
-from sklearn.imputeimport SimpleImputer
+from sklearn.impute import SimpleImputer
 def DataExploration(df):
+    df = df.dropna # drop rows with all colums Nan
     if  2 > len(df.index) or 2 > len(df.columns): # limit the DataSet to Min of 2x2
         return ("Error | The DataSet is too big or too small") # Stop the Script with Error Message
     print(df.dtypes)
-    df.dropna # drop rows with all colums Nan
-
     # Use a breakpoint in the code line below to debug your script.
     #If all Nan
     #If row == row
