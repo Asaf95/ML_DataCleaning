5a6c0dd764f10a8419aad4396ff123040e38dd19
Asaf <asafbm95@gmail.com>
2021-10-26 20:12:32 +0000

    cc

--- main.py
+++ main.py
@@ -3,15 +3,22 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import pandas as pd
+from sklearn.imputeimport SimpleImputer
+def DataExploration(df):
+    if  2 > len(df.index) or 2 > len(df.columns): # limit the DataSet to Min of 2x2
+        return ("Error | The DataSet is too big or too small") # Stop the Script with Error Message
+    print(df.dtypes)
+    df.dropna # drop rows with all colums Nan
 
-def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    #If all Nan
+    #If row == row
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    print("h")
-    print('ss')
+    df = pd.read_csv('test.csv')
+    print(df)
+    print(DataExploration(df))
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
